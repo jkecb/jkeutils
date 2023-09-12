27c0a77e2cde7fe5a89c329ef2c3bfa9a725ca8b
jkecb <13311179+jkecb@users.noreply.github.com>
2023-09-12 20:11:07 +0800

    initial commit with ExponentialBackoffRequests, openai_post, and translate.

---
+++ openai_utils.py
@@ -0,0 +1,43 @@
+import json
+import os
+from requestsss import ExponentialBackoffRequest  # Assuming this is the name of the other file
+
+def openai_post(user_message, system_message="", model="gpt-3.5-turbo"):
+    openai_api_key = os.environ.get("OPENAI_API_KEY")
+    if not openai_api_key:
+        raise ValueError("Please set the OPENAI_API_KEY environment variable.")
+
+    endpoint = "https://api.openai.com/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {openai_api_key}"
+    }
+    messages = [{"role": "user", "content": user_message}]
+
+    if system_message:
+        messages.insert(0, {"role": "system", "content": system_message})
+
+    payload = {
+        "model": model,
+        "messages": messages
+    }
+
+    requester = ExponentialBackoffRequest()
+    response = requester.post(endpoint, headers=headers, data=json.dumps(payload))
+
+    if response.status_code == 200:
+        response_data = response.json()
+        response_data["response"] = response_data["choices"][-1]["message"]["content"].strip()
+        return response_data
+    else:
+        response.raise_for_status()
+
+def translate(text, target_language="Simplified Chinese"):
+    system_message = "You are a professional translator. You translate accurately, fluently and reliably."
+    user_message = f"Translate to {target_language}, return only translated content, don't include original text. Text to be translated:\n{text}"
+
+    response_data = openai_post(user_message,system_message)
+    translation = response_data.get("response", "")
+
+    return translation
+
---
+++ requestsss.py
@@ -0,0 +1,44 @@
+import time
+import random
+import requests
+
+# Some districts need this.
+#
+# # Usage:
+# requester = requestsss.ExponentialBackoffRequest()
+# response = requester.get("https://example.com/api/data")
+# print(response.json())
+
+class ExponentialBackoffRequest:
+    def __init__(self, max_retries=5, base_delay=0.125, max_delay=16.0, backoff_factor=2):
+        self.max_retries = max_retries
+        self.base_delay = base_delay
+        self.max_delay = max_delay
+        self.backoff_factor = backoff_factor
+
+    def get(self, url, **kwargs):
+        return self._request_with_backoff("GET", url, **kwargs)
+
+    def post(self, url, **kwargs):
+        return self._request_with_backoff("POST", url, **kwargs)
+
+    def _request_with_backoff(self, method, url, **kwargs):
+        retries = 0
+        while retries <= self.max_retries:
+            try:
+                response = requests.request(method, url, **kwargs)
+                response.raise_for_status()  # Raises an exception for 4xx and 5xx responses.
+                return response
+            except requests.RequestException as e:
+                # If the error is a 4xx client error, raise it without retrying
+                if isinstance(e, requests.HTTPError) and 400 <= e.response.status_code < 500:
+                    raise e
+
+                wait = min(self.max_delay, (self.backoff_factor ** retries) * self.base_delay)
+                print(e)
+                print(f'Retrying {retries}...')
+                jitter = wait * random.uniform(0.5, 1.5)  # Add randomness
+                time.sleep(jitter)
+                retries += 1
+
+        raise requests.RequestException(f"Failed after {self.max_retries} retries")
